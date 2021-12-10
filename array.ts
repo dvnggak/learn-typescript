9ec079668722a01ee834cba4cc0c6be3e536f502
DevaK25 <devanggakertawijaya@gmail.com>
2021-12-10 08:26:57 +0700

    create array.ts file

---
+++ array.ts
@@ -0,0 +1,34 @@
+// THERE IS TWO WAY FOR US TO DECLARING ARRAY
+    // FIRST WAY IS SIMILIAR WITH OTHER DATA TYPE DECLARATION
+let names : string [] = ["Dev", "Angga", "Kertawijaya"];
+    // SECOND WAY IS DIFFERENT WITH THE FIRST ONE, CAUSE WE NEED TO SET ARRAY SYNTAX INSIDE THE NUMBERS VARIABLE
+    // IN THIS WAY WE CAN BE MORE CLEAR FOR WHAT IS THE DATA TYPE
+let numbers : Array<number> = [1,2,3,4,5,];
+
+
+// WE CAN ALSO HAVE AN ARRAY THAT CONTAINS DIFFERENT DATA TYPE 
+// THAT WE CALLED TUPLE , AN ARRAY THAT CONTAINS VARIETIES OF VARIEBLE IN ONE ARRAY
+    // FOR EXAMPLE
+let student : [string, string, number] = ["Dev", "Angga", 20];
+    // from student variable, we can see that it have 2 string and 1 number of data type
+    // for that we need to set the value base on the data type we make.
+
+// PRINT THE DATA OF ARRAY AND TUPLE
+    // ARRAY AND TUPLE HAVE THE SAME WAY TO CALL THE VALUE BY LOGING THE ARRAY VARIABLE AND THE INDEX OF THE VALUE WE NEED
+// console.log(names[0]);
+// console.log(names[1]);
+// console.log(numbers[4]);
+// console.log(student[2]);
+
+// WE CAN CHANGE THE VALUE OF INDEX WE WANT TO CHANGE INSIDE AN ARRAY 
+// WITH RE-INITIALITATIONS THE VALUE
+// for example
+names[0] = "Devangga";
+names[1] = "Dev";
+numbers[4] = 0;
+student[1] = "Devangga"; 
+
+console.log(names[0]);
+console.log(names[1]);
+console.log(numbers[4]);
+console.log(student[1]);
