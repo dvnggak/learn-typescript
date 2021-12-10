5456bcbb84360793bbe2930d03faa3ae8c0188ae
DevaK25 <devanggakertawijaya@gmail.com>
2021-12-10 09:12:29 +0700

    create enum.ts file

---
+++ enum.ts
@@ -0,0 +1,14 @@
+enum gender {male = 1, female = 2, undefined = 0};
+
+let jenisKelamin : gender = gender.male;
+console.log(jenisKelamin);
+
+// ENUM STRING
+enum category {
+    grocery = "GROCERY",
+    electronic = "ELECTRONIC",
+    handphone = "HANDPHONE"
+};
+
+let kategori : category = category.grocery;
+console.log(kategori);
